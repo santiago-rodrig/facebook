40ae7bc8cd7027f1f2313b4ca3c5c9022bbf0f84
Santiago <santo1996.29@gmail.com>
2020-01-29 13:55:27 -0400

    better leave as it is

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,5 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
-    confirmations: 'users/confirmations',
-    passwords: 'users/passwords',
-    registrations: 'users/registrations',
-    sessions: 'users/sessions',
-    unlocks: 'users/unlocks'
   }
 end
