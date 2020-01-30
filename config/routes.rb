a3cf00d4424d1e15d98f5f16168260a7a201e461
Santiago <santo1996.29@gmail.com>
2020-01-30 02:49:01 -0400

    sign out to new session path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions: 'users/sessions'
   }
 
   resources :users, only: [:show, :edit, :update]
