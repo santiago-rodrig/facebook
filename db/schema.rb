fafcc673f0f1372104c49ca6e5f1fd97302bec6b
Santiago <santo1996.29@gmail.com>
2020-01-31 18:14:09 -0400

    created and migrated likes table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200129214627) do
+ActiveRecord::Schema.define(version: 20200131221007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "liker_id"
+    t.bigint "liked_post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["liked_post_id"], name: "index_likes_on_liked_post_id"
+    t.index ["liker_id", "liked_post_id"], name: "index_likes_on_liker_id_and_liked_post_id", unique: true
+    t.index ["liker_id"], name: "index_likes_on_liker_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
