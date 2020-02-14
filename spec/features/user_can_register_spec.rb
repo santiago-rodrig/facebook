c2c56dfdaf2df85383b37b6784a8e8b1b5b94f69
Santiago Rodríguez <santo1996.29@gmail.com>
2020-02-14 18:49:45 -0400

    little issue in helper call, user registration spec

---
+++ spec/features/user_can_register_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "UserCanRegisters", type: :feature do
+  it 'registers a user' do
+    users_count = User.count
+    visit(new_user_registration_path)
+    expect(page).to have_current_path(new_user_registration_path)
+    fill_in('Email', with: 'bob@example.com')
+    fill_in('First name', with: 'bob')
+    fill_in('Last name', with: 'sinclair')
+    fill_in('Password', with: 'secret')
+    fill_in('Password confirmation', with: 'secret')
+    click_button('Sign up')
+    expect(User.count).to eq(users_count + 1)
+  end
+end
