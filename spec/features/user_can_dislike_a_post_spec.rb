c3a49417bb098d18d0847d4e5dbc115ca7f39734
Santiago Rodríguez <santo1996.29@gmail.com>
2020-02-16 10:33:41 -0400

    user can dislike a post feature spec

---
+++ spec/features/user_can_dislike_a_post_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "UserCanDislikeAPosts", type: :feature do
+  let(:user) do
+    User.create(
+      email: 'bob@example.com',
+      first_name: 'bob',
+      last_name: 'sinclair',
+      password: 'secret',
+      password_confirmation: 'secret'
+    )
+  end
+
+  let(:post) do
+    user.posts.create(
+      title: 'This post',
+      content: 'Is original'
+    )
+  end
+
+  before do
+    sign_in(user)
+  end
+
+  it 'dislikes a post' do
+    visit(post_path(post))
+    find_link(href: like_post_path(post_id: post.id)).click
+    like_count = post.likers.count
+    find_link(href: unlike_post_path(post_id: post.id)).click
+    expect(page).to have_current_path(post_path(post))
+    [post, user].each { |e| e.reload }
+    expect(post.likers.count).to eq(like_count - 1)
+  end
+end
